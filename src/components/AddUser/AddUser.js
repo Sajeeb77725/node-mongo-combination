eb7b1d1e1c759b5d919014997aa5f1ffddfeb17f
Sajeeb77725 <mandal15-4045@diu.edu.bd>
2023-02-14 01:50:15 +0600

    added form for create user

--- src/components/AddUser/AddUser.js
+++ src/components/AddUser/AddUser.js
@@ -1,9 +1,20 @@
 import React from "react";
 
 const AddUser = () => {
+  const handleAddUser = (event) => {
+    event.preventDefault();
+    const name = event.target.name.value;
+    const email = event.target.email.value;
+    const user = { name, email };
+  };
   return (
     <div>
       <h2>Add a user</h2>
+      <form onSubmit={handleAddUser}>
+        <input type="text" name="name" placeholder="Name" id="" required />
+        <input type="email" name="email" placeholder="Email" required id="" />
+        <input type="submit" value="Add User" />
+      </form>
     </div>
   );
 };
