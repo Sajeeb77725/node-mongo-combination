864d23a3ca53bef240ed0d937d01e86d16ebdf15
Sajeeb77725 <mandal15-4045@diu.edu.bd>
2023-02-14 23:53:24 +0600

    got data from backend

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -1,9 +1,23 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 
 const Home = () => {
+  const [users, setUsers] = useState([]);
+  useEffect(() => {
+    fetch("http://localhost:5000/user")
+      .then((res) => res.json())
+      .then((data) => setUsers(data));
+  }, []);
   return (
     <div>
       <h2>This is Home.</h2>
+      <h3>Available Users: {users.length}</h3>
+      <ul>
+        {users.map((user) => (
+          <li key={user._id}>
+            {user.name} : {user.email}
+          </li>
+        ))}
+      </ul>
     </div>
   );
 };
